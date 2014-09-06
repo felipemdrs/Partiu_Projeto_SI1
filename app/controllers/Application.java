dbc1dd9b4080661a701a0cca5742eeaf2b556904
Felipe Miranda <felipe.miranda.mdrs@gmail.com>
2014-09-05 23:48:25 -0300

    Main pages linked with left menu

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,19 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+	public static Result index() {
+        return ok(views.html.index.render("Your new application is ready."));
     }
-
+	
+	public static Result travels_all() {
+        return ok(views.html.home.travel_list.index.render());
+    }
+	
+	public static Result travels_board() {
+        return ok(views.html.home.travel_list.index.render());
+    }
+	
+	public static Result travels_admin() {
+        return ok(views.html.home.travel_list.index.render());
+    }
 }
