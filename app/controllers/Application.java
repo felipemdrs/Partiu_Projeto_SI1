a0794ce6608305f20bf838efbf7386b9f69a175c
Felipe Miranda <felipe.miranda.mdrs@gmail.com>
2014-09-08 01:42:10 -0300

    Home interface has created

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
 	public static Result index() {
-        return redirect(routes.TravelController.list());
+		return ok(views.html.index.render());
     }
 	
 }
