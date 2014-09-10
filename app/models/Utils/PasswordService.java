f2da708025c204a1a337cc376cc6411a6bfcdc74
Abner Matheus <abner.math.c@gmail.com>
2014-09-10 07:39:15 -0300

    Big changes on UI

--- app/models/Utils/PasswordService.java
+++ app/models/Utils/PasswordService.java
@@ -3,47 +3,37 @@ package models.Utils;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import org.apache.directory.api.util.Base64;
 //import java.util.Base64;
 
 
-public final class PasswordService
-{
+public final class PasswordService{
   private static PasswordService instance;
 
-  private PasswordService()
-  {
+  private PasswordService() {
   }
 
-  public String encrypt(String plaintext) throws Exception
-  {
+  public String encrypt(String plaintext) throws Exception{
     MessageDigest md = null;
-    try
-    {
+    try {
       md = MessageDigest.getInstance("SHA");
-    }
-    catch(NoSuchAlgorithmException e)
-    {
+    } catch(NoSuchAlgorithmException e) {
       throw new Exception(e.getMessage());
-    }
-    try
-    {
+    } 
+    try{
       md.update(plaintext.getBytes("UTF-8"));
-    }
-    catch(UnsupportedEncodingException e)
-    {
+    }catch(UnsupportedEncodingException e){
       throw new Exception(e.getMessage());
     }
 
     byte raw[] = md.digest();
-    //String hash = Base64.getEncoder().encodeToString(raw);
-	String hash = null;    
+    String hash = String.valueOf(Base64.encode(raw));  
 	return hash;
   }
   
-  public static PasswordService getInstance()
-  {
-    if(instance == null)
-    {
+  public static PasswordService getInstance(){
+    if(instance == null){
        instance = new PasswordService(); 
     } 
     return instance;
