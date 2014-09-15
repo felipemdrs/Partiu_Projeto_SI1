ed9e379c3b645cc5d4f002598b782f0ff2075e87
Felipe Miranda <felipe.miranda.mdrs@gmail.com>
2014-09-15 01:17:16 -0300

    Fix bug in special char

--- public/javascripts/custom/join-travel.js
+++ public/javascripts/custom/join-travel.js
@@ -49,7 +49,7 @@ function clickJoinButton() {
 
 		$.proxy($.ajax({
 			type: 'put',
-			url: '/travels/' + travelId + '/join/' + $("#typePasswordModal input").val(),
+			url: '/travels/' + travelId + '/join/' + escape($("#typePasswordModal input").val()),
 			success: function(result) {
 				enableJoinButton(button);
 			}
@@ -131,3 +131,18 @@ $(function(){
 	hoverJoinButton();
 	joinTravel();
 });
+
+(function($){
+    $.getQuery = function( query ) {
+        query = query.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+        var expr = "[\\?&]"+query+"=([^&#]*)";
+        var regex = new RegExp( expr );
+        var results = regex.exec( window.location.href );
+        if( results !== null ) {
+            return results[1];
+            return decodeURIComponent(results[1].replace(/\+/g, " "));
+        } else {
+            return false;
+        }
+    };
+})(jQuery);
